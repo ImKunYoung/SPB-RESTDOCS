0c51677796c6212fedff48fbdac31a3b5d63576a
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-27 23:04:11 +0900

    initial commit

---
+++ src/main/java/com/example/spbrestdocs/SpbRestdocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spbrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbRestdocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbRestdocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spbrestdocs/SpbRestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spbrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbRestdocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
